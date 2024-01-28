4d5cfef37d4f27def06226db196e0667f37ef3ae
Starlin De La Cruz <starlinr82@gmail.com>
2024-01-28 12:30:08 -0400

    Fix: valid-object-id

--- src/helpers/valide-object-Id.ts
+++ src/helpers/valide-object-Id.ts
@@ -1,9 +1,11 @@
 import { BadRequestException, NotFoundException } from "@nestjs/common";
 import { Types } from "mongoose";
 
-export function validateObjectId(id: string) {
+export async function validateObjectId(id: string) {
     if (!Types.ObjectId.isValid(id)) {
         throw new NotFoundException("ID not found")
     }
+
+    return Promise.resolve(true)
 }
 
